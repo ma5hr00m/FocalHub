3e15ef512a06423a68e008abe1e52a9529625bc6
Super User <3411281455@qq.com>
2024-08-25 00:40:09 +0800

    fix: remove client

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,10 +2,7 @@ package routes
 
 import (
 	"fmt"
-	"focalhub/internal/client"
 	"focalhub/internal/config"
-	"io/fs"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,13 +10,6 @@ import (
 
 func Run() {
 	r := gin.Default()
-
-	// client fs embed
-	fe, err := fs.Sub(client.FS, "dist")
-	if err != nil {
-		log.Fatal("Failed to sub path `dist`: %v", err)
-	}
-	r.StaticFS("/m", http.FS(fe))
 
 	// server
 	r.GET("/ping", func(c *gin.Context) {
@@ -29,7 +19,7 @@ func Run() {
 	})
 
 	port := config.Conf.App.Port
-	err = r.Run(fmt.Sprintf(":%d", port))
+	err := r.Run(fmt.Sprintf(":%d", port))
 	if err != nil {
 		panic(fmt.Errorf("fatal error when starting server: %s", err))
 	}
