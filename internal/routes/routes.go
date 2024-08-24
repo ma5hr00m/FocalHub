b0b9c9ce9c832d9d55c92ae7301cebaa98c4d6c1
Super User <3411281455@qq.com>
2024-08-25 00:32:34 +0800

    feat: go fs embed

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,7 +2,10 @@ package routes
 
 import (
 	"fmt"
+	"focalhub/internal/client"
 	"focalhub/internal/config"
+	"io/fs"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -11,6 +14,14 @@ import (
 func Run() {
 	r := gin.Default()
 
+	// client fs embed
+	fe, err := fs.Sub(client.FS, "dist")
+	if err != nil {
+		log.Fatal("Failed to sub path `dist`: %v", err)
+	}
+	r.StaticFS("/m", http.FS(fe))
+
+	// server
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello, world!",
@@ -18,7 +29,7 @@ func Run() {
 	})
 
 	port := config.Conf.App.Port
-	err := r.Run(fmt.Sprintf(":%d", port))
+	err = r.Run(fmt.Sprintf(":%d", port))
 	if err != nil {
 		panic(fmt.Errorf("fatal error when starting server: %s", err))
 	}
