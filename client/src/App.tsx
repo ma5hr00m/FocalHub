556a32b6940cdee066e66345580ea1c4f64bdc84
root <3411281455@qq.com>
2024-08-26 22:58:45 +0800

    fix: TypeScript check

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useAtom } from 'jotai';
 import { configAtom } from './jotai/jotai';
 import AppRoutes from './routes/AppRoutes';
